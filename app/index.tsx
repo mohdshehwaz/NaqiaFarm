5787729633a12e8657ac892ac1bad26dbcbf0310
mohdshehwaz <mohdshehwaz@gmail.com>
2026-04-26 12:35:38 +0530

    add login

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,39 @@
-import { Redirect, Href } from "expo-router";
+import { useEffect, useState } from "react";
+import { useRouter } from "expo-router";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { View, ActivityIndicator } from "react-native";
 
 export default function Index() {
-  return <Redirect href={"/login" as Href} />;
+  const router = useRouter();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const checkLogin = async () => {
+      try {
+        const token = await AsyncStorage.getItem("token");
+
+        if (token) {
+          router.replace("/(tabs)/home");
+        } else {
+          router.replace("/(auth)/login");
+        }
+      } catch (error) {
+        router.replace("/(auth)/login");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    checkLogin();
+  }, []);
+
+  if (loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
+  return null;
 }
